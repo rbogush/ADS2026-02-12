0d3998dec2816c15c5caf11ae8dbb4cfe7f7dc4c
Roman <romanbogus735@gmail.com>
2026-03-22 17:49:25 +0300

    A_huffman done

--- src/by/it/group551001/bogush/lesson03/A_Huffman.java
+++ src/by/it/group551001/bogush/lesson03/A_Huffman.java
@@ -68,20 +68,37 @@ public class A_Huffman {
         Map<Character, Integer> count = new HashMap<>();
         //1. переберем все символы по очереди и рассчитаем их частоту в Map count
         //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
-
+        for(int i = 0; i < s.length(); i++) {
+            count.merge(s.charAt(i), 1, Integer::sum);
+        }
         //2. перенесем все символы в приоритетную очередь в виде листьев
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        for (Map.Entry<Character, Integer> entry : count.entrySet()) {
+            priorityQueue.add(new LeafNode(entry.getValue(), entry.getKey()));
+        }
 
         //3. вынимая по два узла из очереди (для сборки родителя)
         //и возвращая этого родителя обратно в очередь
         //построим дерево кодирования Хаффмана.
         //У родителя частоты детей складываются.
-
+        while (priorityQueue.size() > 1) {
+            Node left = priorityQueue.poll(); // вынимаем наименьший
+            Node right = priorityQueue.poll(); // вынимаем второй наименьший
+            priorityQueue.add(new InternalNode(left, right)); // создаём родителя и кладём обратно
+        }
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
+        Node root = priorityQueue.poll();
+        if (root instanceof LeafNode) {
+            root.fillCodes("0");
+        } else {
+            root.fillCodes("");
+        }
         StringBuilder sb = new StringBuilder();
         //.....
-
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(codes.get(s.charAt(i)));
+        }
         return sb.toString();
         //01001100100111
         //01001100100111
